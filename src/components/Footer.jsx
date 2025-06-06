48cd0eb33edb9b53589826d729215f39d0ab3174
MirnaBuntic <mirnacb@hiof.no>
2025-06-07 01:53:04 +0200

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -7,7 +7,7 @@ export default function Footer() {
                     <a href="mailto:mirnacb@hiof.no"><i className="fas fa-envelope"></i>mirnacb@hiof.no</a>
                 </li>
                 <li>
-                    <a href="#"><i className="fab fa-linkedin"></i>Mirna Buntic</a>
+                    <a href="https://linkedin.com/in/mirna-buntic-83220036a"><i className="fab fa-linkedin"></i>Mirna Buntic</a>
                 </li>
             </ul>
         </footer>
